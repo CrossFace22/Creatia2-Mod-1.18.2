c805094d8360e62595a28feb45937b53b1625574
CrossFace22 <crossmct@gmail.com>
2022-07-09 19:31:33 -0300

    Initial commit

---
+++ src/main/java/net/losvagos/creatia2/Creatia2.java
@@ -0,0 +1,38 @@
+package net.losvagos.creatia2;
+
+import com.mojang.logging.LogUtils;
+import net.losvagos.creatia2.item.CreatiaItems;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(Creatia2.MOD_ID)
+public class Creatia2
+{
+    public static final String MOD_ID = "creatia2";
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public Creatia2()
+    {
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        CreatiaItems.register(eventBus);
+        eventBus.addListener(this::setup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        // some preinit code
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
---
+++ src/main/java/net/losvagos/creatia2/item/CreatiaItems.java
@@ -0,0 +1,51 @@
+package net.losvagos.creatia2.item;
+
+import net.losvagos.creatia2.Creatia2;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class CreatiaItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, Creatia2.MOD_ID);
+
+    public static final RegistryObject<Item> CLAY_CUP = ITEMS.register("clay_cup",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).stacksTo(1)));
+
+    public static final RegistryObject<Item> HARDENED_CLAY_CUP = ITEMS.register("hardened_clay_cup",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).stacksTo(1)));
+
+    public static final RegistryObject<Item> CHOCOLATE_CUP = ITEMS.register("chocolate_cup",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).stacksTo(1)));
+
+    public static final RegistryObject<Item> COFFEE_CUP = ITEMS.register("coffee_cup",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).stacksTo(1)));
+
+    public static final RegistryObject<Item> BUILDERS_TEA_CUP = ITEMS.register("builders_tea_cup",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).stacksTo(1)));
+
+    public static final RegistryObject<Item> BLACK_TEA_CUP = ITEMS.register("black_tea_cup",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).stacksTo(1)));
+
+    public static final RegistryObject<Item> DANDELION_TEA_CUP = ITEMS.register("dandelion_tea_cup",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).stacksTo(1)));
+
+    public static final RegistryObject<Item> GREEN_TEA_CUP = ITEMS.register("green_tea_cup",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).stacksTo(1)));
+
+    public static final RegistryObject<Item> PURULENT_TEA_CUP = ITEMS.register("purulent_tea_cup",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).stacksTo(1)));
+
+    public static final RegistryObject<Item> ROSE_HIP_TEA_CUP = ITEMS.register("rose_hip_tea_cup",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).stacksTo(1)));
+
+    public static final RegistryObject<Item> YELLOW_TEA_CUP = ITEMS.register("yellow_tea_cup",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).stacksTo(1)));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
